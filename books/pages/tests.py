2fb1c8584c1d557fabe506af372acb006b91efdd
Karatug Ozan Bircan <karatugo@gmail.com>
2020-05-25 12:35:59 +0300

    Add unit test for template

--- books/pages/tests.py
+++ books/pages/tests.py
@@ -9,3 +9,7 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
